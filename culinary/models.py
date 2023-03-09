ddd79f044d35597650161c3c886b86dc92bb553a
Gvard Windlass <gvard.windlass@gmail.com>
2023-03-09 11:25:10 +0300

    Add ingredient and measure models

--- culinary/models.py
+++ culinary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
