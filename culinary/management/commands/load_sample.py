2104dddf2b216121f49d9b9e0c55b38a2d6fff20
Gvard Windlass <gvard.windlass@gmail.com>
2023-03-14 11:12:17 +0300

    Add Fridge to sample generation

--- culinary/management/commands/load_sample.py
+++ culinary/management/commands/load_sample.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from test.factories import IngredientFactory, UserFactory
+from test.factories import IngredientFactory, UserFactory, FridgeFactory
 
 
 class Command(BaseCommand):
@@ -13,6 +13,10 @@ class Command(BaseCommand):
         user1 = UserFactory.create()
         user2 = UserFactory.create()
 
-        IngredientFactory.create_batch(21, user_id=staff_user.id)
-        IngredientFactory.create_batch(5, user_id=user1.id)
-        IngredientFactory.create_batch(5, user_id=user2.id)
+        staff_ingrs = IngredientFactory.create_batch(21, user_id=staff_user.id)
+        user1_ingrs = IngredientFactory.create_batch(5, user_id=user1.id)
+        user2_ingrs = IngredientFactory.create_batch(5, user_id=user2.id)
+
+        FridgeFactory.create(user_id=staff_user.id, shelf=staff_ingrs)
+        FridgeFactory.create(user_id=user1.id, shelf=user1_ingrs)
+        FridgeFactory.create(user_id=user2.id, shelf=user2_ingrs)
